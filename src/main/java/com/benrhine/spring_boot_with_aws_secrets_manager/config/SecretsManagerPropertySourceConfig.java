4f46ef974ac44b5ba49a512dd2bac3a486cd9ee5
Ben Rhine <rhine.ben@gmail.com>
2025-05-21 11:45:04 -0600

    lowering to spring 2.7.x version, tested with both java 8 and 11

--- src/main/java/com/benrhine/spring_boot_with_aws_secrets_manager/config/SecretsManagerPropertySourceConfig.java
+++ src/main/java/com/benrhine/spring_boot_with_aws_secrets_manager/config/SecretsManagerPropertySourceConfig.java
@@ -1,10 +1,10 @@
 package com.benrhine.spring_boot_with_aws_secrets_manager.config;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.ConfigurableEnvironment;
 
+import javax.annotation.PostConstruct;
 
 @Configuration
 public class SecretsManagerPropertySourceConfig {
